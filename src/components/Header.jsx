488ded770e66febc0ea48cb7a4aabbb8463adbfb
Kamogelo <ekmogasoa@gmail.com>
2025-04-20 21:07:37 +0200

    fixed navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <h1>
           {h1} {"🔰"}
         </h1>
-        <ul className="flex justify-center md:border-1 p-1.5 rounded-full border-white items-center space-x-5 sticky top-0 bg-black/30 backdrop-blur-sm right-2/5 md:transform md:-translate-x-1/12 sm:px-8">
+        <ul className="flex justify-center md:border-1 p-1.5 rounded-full border-white items-center lg:space-x-15 space-x-5 sticky top-0 bg-black/30 backdrop-blur-sm right-2/5 lg:transform/12 xl:translate-x-1/12  sm:px-8 cursor-pointer">
           <li>home</li>
           <li>contact</li>
           <li>about us</li>
